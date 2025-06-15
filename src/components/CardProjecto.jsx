f608c0e84259a132f12c57d5de0007815d590a66
Jesus Pedro Bapolo <jesusestudante@gmail.com>
2025-06-15 04:35:13 +0100

    set cardProjecto width

--- src/components/CardProjecto.jsx
+++ src/components/CardProjecto.jsx
@@ -1,10 +1,10 @@
 function CardProjecto() {
     return (
-        <li className="border border-gray-300" style={{flexBasis: "2000px"}}>
+        <li className="min-w-[200px] max-w-[200px] flex-shrink-0 border border-gray-300 rounded-md bg-white shadow-md">
             <div className="bg-gray-500 h-20 text-center text-white">image</div>
             <section className="p-2 ">
-                <h3>titulo</h3>
-                <p>Descriçao</p>
+                <h3>Loja Virtual</h3>
+                <p>Descriçao hdjkfjkdshfjkdhfjksdhjfgdjs syfiuwebf jwekhn fjweh fwebfjebwewih wehfj</p>
                 <button className="flex items-center justify-center py-2 px-3 bg-[#4285F4] hover:bg-[#668cca] rounded-md text-white cursor-pointer transition-all shadow-xl">ver projecto</button>
             </section>
         </li>
