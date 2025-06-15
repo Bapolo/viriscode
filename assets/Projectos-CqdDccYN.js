8e8c81e4d8cd20bbc183faa3433db8b34c181383
Jesus Pedro Bapolo <jesusestudante@gmail.com>
2025-06-15 04:16:53 +0100

    Updates

--- assets/Projectos-CqdDccYN.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Hn635jFK.js";function r(){return o.jsx("h1",{children:"Nossos Projectos"})}export{r as default};
--- assets/Servicos-BXg14wqP.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Hn635jFK.js";function r(){return s.jsx("h1",{children:"Nossos Servicos"})}export{r as default};
